28dfff54da1dd337626bc44b2aac326614e66969
Patrick Miller <paddymills@proton.me>
2023-07-24 15:24:19 -0400

    refactor api per todos

--- src/api/jobship.rs
+++ src/api/jobship.rs
@@ -7,39 +7,46 @@ use std::{
 };
 
 const JOBSHIP_RE: LazyCell<Regex> = LazyCell::new(|| {
-    Regex::new(r"^(\d{7}[[:alpha:]])-(\d+)$").expect("failed to build regex")
+    Regex::new(r"^(\d{7})([[:alpha:]]{1})-(\d+)$")
+        .expect("failed to build JobShipment parsing regex")
 });
 
 /// Job and Shipment
 #[derive(Clone, Debug, Default, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct JobShipment {
-    /// Job number (with structure letter)
-    // TODO: split job into project and structure
-    pub job: String,
+    /// Job number
+    pub job: u32,
+    
+    /// Structure letter
+    pub structure: String,
+
     /// Shipment number
-    // TODO: refactor as number
     pub shipment: u8
 }
 
 impl FromStr for JobShipment {
-    type Err = std::fmt::Error;
+    type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match JOBSHIP_RE.captures(s) {
             Some(cap) => {
-                Ok(
-                    Self {
-                        // TODO: handle the unwraps
-                        job: cap.get(1).unwrap().as_str().to_uppercase().into(),
-                        shipment: cap.get(2).unwrap().as_str().parse().unwrap(),
-                    }
-                )
+                // `cap.get(n).unwrap()` should not fail
+                let job = match cap.get(1).unwrap().as_str().parse() {
+                    Ok(job) => job,
+                    Err(_) => return Err(format!("Failed to parse Job from `{}`", s))
+                };
+                let structure = cap.get(2).unwrap().as_str().into();
+                let shipment = match cap.get(3).unwrap().as_str().parse() {
+                    Ok(job) => job,
+                    Err(_) => return Err(format!("Failed to parse Shipment from `{}`", s))
+                };
+                
+                Ok( Self { job, structure, shipment } )
             },
             None => {
-                eprintln!("Failed to parse job-shipment: {}", s);
+                error!("Failed to parse JobShipment from `{}`", s);
 
-                // TODO: don't panic (custom error?)
-                panic!("invalid job-shipment")
+                Err(format!("Failed to parse Job, Structure and Shipment from `{}`", s))
             }
         }
     }
@@ -47,6 +54,6 @@ impl FromStr for JobShipment {
 
 impl Display for JobShipment {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}-{}", self.job, self.ship)
+        write!(f, "{}{}-{}", self.job, self.structure, self.shipment)
     }
 }
