e5c24624cb6089846ee894b9f8fc1c690d62be1c
Patrick Miller <paddymills@proton.me>
2023-07-25 12:02:07 -0400

    more linting directives

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 
+// linting directives (see https://doc.rust-lang.org/rustc/lints/index.html)
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
-#![warn(unreachable_pub)]
+#![warn(noop_method_call, unreachable_pub)]
 #![warn(variant_size_differences)]
 #![deny(deprecated, legacy_derive_helpers)]
+#![deny(non_ascii_idents)]
 
 //! Middle manager for inventory management
 //! 
