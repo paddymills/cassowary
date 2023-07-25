4a1eb537ac99aaf79fff257f442585e84379d236
Patrick Miller <paddymills@proton.me>
2023-07-25 11:35:18 -0400

    enforce coding style

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
+#![warn(unreachable_pub)]
+#![warn(variant_size_differences)]
+#![deny(deprecated, legacy_derive_helpers)]
 
 //! Middle manager for inventory management
 //! 
