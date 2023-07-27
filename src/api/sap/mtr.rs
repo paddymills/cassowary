a5da5993f206fa80bf55cd13364da07b1f25984b
Patrick Miller <paddymills@proton.me>
2023-07-26 22:02:26 -0400

    add bol

--- src/api/sap/mtr.rs
+++ src/api/sap/mtr.rs
@@ -8,15 +8,18 @@ pub struct MtrIdentifier {
     pub heat: String,
     /// Material PO Number
     pub po: u64,
+    /// Bill of lading
+    pub bol: String
 }
 
-impl TryFrom<(&str, &str)> for MtrIdentifier {
+impl TryFrom<(&str, &str, &str)> for MtrIdentifier {
     type Error = std::num::ParseIntError;
 
-    fn try_from(value: (&str, &str)) -> Result<Self, Self::Error> {
+    fn try_from(value: (&str, &str, &str)) -> Result<Self, Self::Error> {
         Ok( Self {
             heat: String::from( value.0 ),
-            po: value.1.parse()?
+            po: value.1.parse()?,
+            bol: String::from( value.2 )
         } )
     }
 }
@@ -27,9 +30,9 @@ mod tests {
 
     #[test]
     fn test_po_parse() {
-        let test_against = Ok(MtrIdentifier { heat: "D1234".into(), po: 4500123456 });
-        assert_eq!(("D1234", "4500123456").try_into(), test_against);
+        let test_against = Ok(MtrIdentifier { heat: "D1234".into(), po: 4500123456, bol: "11".into() });
+        assert_eq!(("D1234", "4500123456", "11").try_into(), test_against);
         
-        assert!(MtrIdentifier::try_from(("D1234", "4500x")).is_err());
+        assert!(MtrIdentifier::try_from(("D1234", "4500x", "11")).is_err());
     }
 }
