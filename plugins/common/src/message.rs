30a7a166b0a15c610f616c12111df31b8de7d5ae
paddymills <paddymills@pm.me>
2023-08-18 08:53:04 -0400

    add docs

--- plugins/common/src/message.rs
+++ plugins/common/src/message.rs
@@ -1,8 +1,13 @@
 
 
 /// Message type for communicating with the plugin runtime
+// TODO: this should probably be an enum of different message types
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Message {
+    /// message Id number
+    // TODO: maybe use uuid for this
     pub id: u32,
+
+    /// message payload
     pub msg: String
 }
